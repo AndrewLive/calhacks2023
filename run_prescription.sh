81e7ad4ee77dff921e67fb9a971eee5fbb55e89a
Nancy Lau <nancyjlau@gmail.com>
2023-10-29 09:41:12 -0700

    create html to display results and show audio
    Signed-off-by: Nancy Lau <nancyjlau@gmail.com>

--- run_prescription.sh
+++ run_prescription.sh
@@ -4,4 +4,59 @@
 
 latest_image=$(ls -t images/captured_image*.jpg | head -n 1)
 
-./prescription.py "$latest_image"
+output=$(./prescription.py "$latest_image")
+
+cat <<EOF > prescription_output.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prescription Output</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }
+        h1 {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+        img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <h1>Prescription Output</h1>
+    <div class="container">
+        <p>
+            $output
+        </p>
+
+        <audio controls>
+            <source src="prescription_info.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+
+        <img src="$latest_image" alt="Prescription Image">
+        
+    </div>
+</body>
+</html>
+EOF
+
+# Open the generated HTML file in the default web browser on macOS
+open prescription_output.html
